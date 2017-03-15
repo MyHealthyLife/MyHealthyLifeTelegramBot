973506e1ed5991abc9a6e27b34de48057677a6d6
SimoneScalco93 <simone.scalco@studenti.unitn.it>
2017-03-15 17:42:46 +0100

    Added descriptions in the HelpHandler

--- MyHealthyLifeBot/src/main/java/myhealthylife/telegram/bot/handlers/HelpHaldler.java
+++ MyHealthyLifeBot/src/main/java/myhealthylife/telegram/bot/handlers/HelpHaldler.java
@@ -5,26 +5,26 @@ public class HelpHaldler {
 		String message="/help: visualize this message\n";
 		
 		message+="/register <name> <surname> <birthdate>: register to the service\n";
-		message+="/name\n";
-		message+="/surname\n";
-		message+="/birthdate\n";
-		message+="/unregister\n";
+		message+="/name <new name> updates your name\n";
+		message+="/surname <new surname> updates your surname\n";
+		message+="/birthdate <YYYY-MM-DD> updates your birthdate\n";
+		message+="/unregister Deletes your user and all your data in the system\n";
 		message+="/healthstate visualize your current health state\n";
 		message+="/measure_history visualize the history of you measures\n";
 		message+="/addmeasure <type> <value> save a new measure\n";
-		message+="/randomsentence \n";
-		message+="/sentenceforme \n";
-		message+="/foods_for_me\n";
-		message+="/goal\n";
-		message+="/ranking\n";
-		message+="/send\n";
-		message+="/receive\n";
+		message+="/randomsentence Get a random sentence to motivate the user\n";
+		message+="/sentenceforme Get a specific sentence based on your measures\n";
+		message+="/foods_for_me Gets specific type of foods based on your health state\n";
+		message+="/goal Gets the goals you should reach\n";
+		message+="/ranking Gets a compact ranking with the neareast users around your position\n";
+		message+="/send <destination user> <type> <trend (gain or loss)> Sends a sentence to a destination user\n";
+		message+="/receive Receive all the sentences the other users dedicated to you within 7 days\n";
 		message+="/new_recipe\n";
 		message+="/update_recipe\n";
 		message+="/newFood\n";
 		message+="/newGoal\n";
-		message+="/start_notifications\n";
-		message+="/unsubscribe_notification\n";
+		message+="/start_notifications Starts the push notification service for your user\n";
+		message+="/unsubscribe_notification Stops the push notification service for your user\n";
 		
 		return message;
 	}
